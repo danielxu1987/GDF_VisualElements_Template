64fd9e9c445b342e2d0a16bb21188cfae12adc40
s.xu <s.xu@cbs.nl>
2019-10-17 17:14:37 +0200

    Added nodejs folder to try odata out.

---
+++ Nodejs_oData/app.js
@@ -0,0 +1,11 @@
+import { o } from 'odata';
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello Daniel!'))
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
