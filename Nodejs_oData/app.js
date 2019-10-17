941a3147953b6efa1f40f50de95ca2f6a84011aa
s.xu <s.xu@cbs.nl>
2019-10-17 18:11:25 +0200

    Latest version made in the office

--- Nodejs_oData/app.js
+++ Nodejs_oData/app.js
@@ -1,11 +1,21 @@
-import { o } from 'odata';
 
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello Daniel!'))
+//const oData = require('odata');
+const circle = require('./circle');
+const r = 3;
 
+console.log('Circle with radius ${r} has area: ${ circle.area(r) }; ' +
+                ' circumference: ${ circle.circumference(r) }.');
 
+app.get('/', (req, res) => res.send('Hello Daniel!'));
+
+//oData('https://opendata.cbs.nl/ODataApi/odata/37296ned/UntypedDataSet?$select=Perioden,+TotaleBevolking_1')
+//    .get('resource')
+//    .query({ $top: 3 })
+//    .them((data) => data.send())
+    //.then((data) => console.log(data));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ Nodejs_oData/circle.js
@@ -0,0 +1,5 @@
+
+const PI = 3.14159265359;
+
+exports.area = radius => (radius ** 2) * PI;
+exports.circumference = radius => 2 * radius * PI;
